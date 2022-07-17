e61c7888021b24c7ff4d11166c9df4c8b5caf876
Lucas-Casarotti <casarotti@hotmail.com>
2022-07-17 20:37:21 -0300

    1.2

---
+++ 02 - C#/03 - Acesso à dados com C#/01 - Acesso a dados com ADO.NET/DataAccess/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=DESKTOP-3KK17LJ\\SQLEXPRESS;Initial Catalog=Estudo;Integrated Security=True;Encrypt=False";
+            
+            using(var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+                connection.Open();
+
+                using(var command = new SqlCommand())
+                {
+                    command.Connection  = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT NM_Pessoa FROM Pessoas";
+
+
+                    var reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetString(1)}");
+                    }
+                }
+            }
+           
+        }
+    }
+}
