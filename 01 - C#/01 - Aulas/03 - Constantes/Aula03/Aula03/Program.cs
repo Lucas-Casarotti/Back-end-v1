e04b167a1d41ba413637e6882d9b2cc0a7975639
Lucas-Casarotti <casarotti@hotmail.com>
2023-07-28 10:56:32 -0300

    1.6

---
+++ 01 - C#/01 - Aulas/03 - Constantes/Aula03/Aula03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n1 = 2;
+            Console.WriteLine("Valor da constante: " + n1);
+
+        }
+    }
+}
